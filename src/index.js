8b164bb4760df4ec14dacf6f9948c0576f22e6d5
dcrisanto <dorelly.crisanto@gmail.com>
2022-01-01 22:47:51 -0500

    Instalación y estructura de aplicación web react

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+    <App saludo='hi'>
+      <h1>Learm React</h1>
+      <p>Hola</p>
+    </App>,
+  document.getElementById('root')
+);
+
+
