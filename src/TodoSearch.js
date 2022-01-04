28f44d566ef5c590964c39fa36d504e721fe7d1b
dcrisanto <dorelly.crisanto@gmail.com>
2022-01-03 19:21:22 -0500

    Manejo de estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        let value = event.target.value;
+        setSearchValue(value);
     };
 
-    return(
+    return[
         <input 
         onChange = {onSearchValueChange}
-        className="TodoSearch" placeholder='buscar tareas' />
-    );
+        className="TodoSearch" placeholder='buscar tareas' 
+        value={searchValue}
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export { TodoSearch };
