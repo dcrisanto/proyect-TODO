934a0114ecfba9750976a94fc5781f90a65c3c99
dcrisanto <dorelly.crisanto@gmail.com>
2022-01-12 19:13:26 -0500

    render functions

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './TodoList.css';
 
 function TodoList(props) {
+    const renderFunc = props.children || props.render;
+
     return(
         <section className="TodoList-container">
             {/* Si recibe una props error, loading renderizará lo que viene en la render prop onError, onLoading*/}
@@ -9,7 +11,7 @@ function TodoList(props) {
             {props.loading && props.onLoading()}
             {(!props.loading && !props.totalTodos) && props.onEmptyTodos()}
             {(!!props.totalTodos && !props.searchedTodos.length) && props.onEmptySearchResult()}
-            {props.searchedTodos.map(props.render)}
+            {props.searchedTodos.map(renderFunc)}
             <ul>
                 {props.children}
             </ul>
